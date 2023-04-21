d7430f28e6c993874619b882c398e6bb82c5bed7
scopalaffairs <ministry@scopal-affairs.com>
2023-04-21 15:52:37 +0200

    Update query to cleaned table
    hive_metastore.sit.near_social_txs_clean

--- near_recommender/src/data/queries/query_get_post_text.py
+++ near_recommender/src/data/queries/query_get_post_text.py
@@ -4,7 +4,7 @@ SELECT
   post:main:text AS post_text,
   block_timestamp
 FROM
-  hive_metastore.mainnet.silver_near_social_txs_parsed
+  hive_metastore.sit.near_social_txs_clean
 WHERE
   post:main:text IS NOT NULL;
 """
