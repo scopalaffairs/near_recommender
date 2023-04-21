e12e5b8761f7ccdffdd43e480c8d1aa876620aa3
scopalaffairs <ministry@scopal-affairs.com>
2023-04-21 18:23:48 +0200

    Outline logical board of functions for main implementation

--- near_recommender/src/main.py
+++ near_recommender/src/main.py
@@ -8,7 +8,8 @@ from near_recommender.src.models.similar_posts import get_similar_post_users
 from near_recommender.src.models.similar_tags import get_similar_tags_users
 
 
-
+NEW_ACCOUNT_AGE = 7 # in days, TODO: refine
+TIME_DEF = 60 # in days, TODO: refine
 
 
 def task1():
@@ -31,25 +32,26 @@ def main(df):
     -> Trending Users Table
     Recommend Trending User to New User
     
-    
     Todo:
         Recommendation system logic:
-        If the user is new (< 1 week, < 3 days):
-        → trending users
-        If the user is not active:
-        → trending users
-        If the user is active:
-        → friends-of-friends
-        If the user has a tag:
-        → tag similarity
-        If the user has posted
-        → post similarity
-        If the user was inactive for some period:
-        → trending users
-        Bot detection (identifies known bots like littlelion, mr27, hypefairy):
-        high % active days
-        high post per day
-        low follow_ratio
+            If the user is new (< 1 week, < 3 days):
+                → trending users
+            If the user is not active:
+                → trending users
+            If the user is active:
+                → friends-of-friends
+            If the user has a tag:
+                → tag similarity
+
+            If the user has posted
+                → post similarity
+            If the user was inactive for some period:
+                → trending users
+
+            Bot detection (identifies known bots like littlelion, mr27, hypefairy):
+                high % active days
+                high post per day
+                low follow_ratio
         …
         but difficult to filter out other account without further analysis of content, etc
         Definition of active users (proposals):
@@ -59,23 +61,31 @@ def main(df):
 
     
     Implemented:
-    
-    Similar Tags can be called independently
-    Similar Posts can be called independently
-    Update Corpus for simlilar posts can be called indep
-    
+        Similar Tags can be called independently
+        Similar Posts can be called independently
+        Update Corpus for simlilar posts can be called indep
     
     Todo Daniel:
-    Posts and Tags rewrite to work with hive_metastore.sit.near_social_txs_clean
-    
-    Todo ...
-    
-    
-    
+        Posts and Tags rewrite to work with hive_metastore.sit.near_social_txs_clean
+        
+    Todo Agustin:
+        Reorganize User metrics table to add all users
+        Separate table for Trending Users (interface for main functions)
     """
     
+    # new users and inactive
+    if address_age < NEW_ACCOUNT_AGE or recency > TIME_DEF:
+        generic_trending_users()
     
-
+    # older users and active ones
+    else:
+        if user_has_tag:
+            output = similar_tags()
+            print(f"Based on your tags, this similar are recommended: {output}")
+        if user_has_posted:
+            similar_posts()
+        else:
+            trending_users_outside_community()
 
 if __name__ == "__main__":
     main(df)
