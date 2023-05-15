23546dbc75113e000393921ca1661a758ba43782
scopalaffairs <ministry@scopal-affairs.com>
2023-05-15 10:40:51 +0200

    Add dbutils to requirements and change module paths for functions

--- near_recommender/__init__.py
+++ near_recommender/__init__.py
@@ -1,5 +1,5 @@
-from src.main import get_recommendations
-from src.models.friends_friends import get_friends_of_friends
-from src.models.similar_posts import get_similar_post_users, update_corpus
-from src.models.similar_tags import get_similar_tags_users
-from src.models.trending_users import get_trending_users
+from near_recommender.src.main import get_recommendations
+from near_recommender.src.models.friends_friends import get_friends_of_friends
+from near_recommender.src.models.similar_posts import get_similar_post_users, update_corpus
+from near_recommender.src.models.similar_tags import get_similar_tags_users
+from near_recommender.src.models.trending_users import get_trending_users
