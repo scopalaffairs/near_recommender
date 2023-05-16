a4c92b50937da192838e219b193944a080f37ec1
scopalaffairs <ministry@scopal-affairs.com>
2023-05-16 18:44:26 +0200

    Fix passing queried last post to similar_post_users

--- near_recommender/src/features/utils.py
+++ near_recommender/src/features/utils.py
@@ -1,7 +1,11 @@
 import pickle
 from typing import List
 
+import pandas as pd
+from pyspark.sql import SparkSession
 from sentence_transformers import SentenceTransformer, util
+
+from near_recommender.src.data.queries.query_get_post_text import query as posts_query
 
 
 def load_pretrained_model(filename: str):
@@ -14,7 +18,16 @@ def save_pretrained_model(filename: str, cxt: str) -> None:
         pickle.dump(cxt, f)
 
 
-def run_update_model(
-    model: SentenceTransformer, corpus: List
-) -> SentenceTransformer:
+def run_update_model(model: SentenceTransformer, corpus: List) -> SentenceTransformer:
     return model.encode(corpus, convert_to_tensor=True)
+
+
+def filter_last_post(signer_id):
+    spark = SparkSession.builder.getOrCreate()
+    result = spark.sql(posts_query)
+    df = result.toPandas()
+    df = df.sort_values(['signer_id', 'block_timestamp'], ascending=[True, False])
+    df = df.drop_duplicates('signer_id')
+    filtered_df = df[df['signer_id'] == signer_id]
+
+    return filtered_df
--- near_recommender/src/main.py
+++ near_recommender/src/main.py
@@ -4,15 +4,19 @@
 
 import json
 from typing import Dict, List
+
 import pandas as pd
 
+from near_recommender.src.features.utils import filter_last_post
 from near_recommender.src.models.friends_friends import get_friends_of_friends
 from near_recommender.src.models.similar_posts import get_similar_post_users
 from near_recommender.src.models.similar_tags import get_similar_tags_users
 from near_recommender.src.models.trending_users import get_trending_users
 
 
-def get_recommendations_per_user(idx: int, user: pd.Series) -> List[Dict]:
+def get_recommendations_per_user(
+    idx: int, user: pd.Series, address_age: int = 7, top_k: int = 5
+) -> List[Dict]:
     """
     Gets all applicable recommendations for a given user.
 
@@ -27,8 +31,8 @@ def get_recommendations_per_user(idx: int, user: pd.Series) -> List[Dict]:
     Returns a list of dictionaries with signer_id and recommended users.
     """
     recommendations = []
-    if user["address_age"] < 7:
-        print(f"new user age < 7: {user['signer_id']}")
+    if user["address_age"] < address_age:
+        print(f"new user {address_age}: {user['signer_id']}")
         trending_users = get_trending_users()
         recommendations.append(trending_users)
     elif user["active_last_month"]:
@@ -37,12 +41,12 @@ def get_recommendations_per_user(idx: int, user: pd.Series) -> List[Dict]:
         recommendations.append(trending_users)
     if user["user_has_tags"]:
         print(f"user has tags: {user['signer_id']}")
-        similar_tags = get_similar_tags_users(idx, top_k=15)
+        similar_tags = get_similar_tags_users(idx, top_k=top_k)
         recommendations.append(similar_tags)
     if user["user_has_posted"]:
         print(f"user posted: {user['signer_id']}")
-        query = """sample string, get actual post string, last one"""
-        similar_post = get_similar_post_users(query, top_k=15)
+        post = filter_last_post(user["signer_id"])
+        similar_post = get_similar_post_users(post.values[0], top_k=top_k)
         recommendations.append(similar_post)
     else:
         print("friends of friends")
