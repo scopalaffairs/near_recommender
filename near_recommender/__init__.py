868a2e6b70eaa46e4b038c0f8935444acb51b698
scopalaffairs <ministry@scopal-affairs.com>
2023-05-16 17:44:49 +0200

    Expose main function

--- near_recommender/__init__.py
+++ near_recommender/__init__.py
@@ -1,4 +1,4 @@
-# from near_recommender.src.main import get_recommendations
+from near_recommender.src.main import get_recommendations_per_user
 from near_recommender.src.models.friends_friends import get_friends_of_friends
 from near_recommender.src.models.similar_posts import get_similar_post_users, update_corpus
 from near_recommender.src.models.similar_tags import get_similar_tags_users
