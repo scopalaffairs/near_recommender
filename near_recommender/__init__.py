d0bc905139bfbcdc772d15caf9a0310b5a826f50
scopalaffairs <ministry@scopal-affairs.com>
2023-05-17 16:49:15 +0200

    Publish docs to github pages

--- near_recommender/__init__.py
+++
@@ -1,5 +0,0 @@
-from near_recommender.src.recommender import get_recommendations_per_user
-from near_recommender.src.models.friends_friends import get_friends_of_friends
-from near_recommender.src.models.similar_posts import get_similar_post_users, update_corpus
-from near_recommender.src.models.similar_tags import get_similar_tags_users
-from near_recommender.src.models.trending_users import get_trending_users
